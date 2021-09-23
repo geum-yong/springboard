7928f40970e205eb44f26ca36047066775b71417
geum-yong <gmyong21@gmail.com>
2021-09-23 22:43:18 +0900

    게시물 조회 구현

--- src/main/java/com/example/springboard/board/controller/BoardController.java
+++ src/main/java/com/example/springboard/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.example.springboard.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -34,5 +35,15 @@ public class BoardController {
     public String insertBoard(BoardDto board) throws Exception {
         boardService.insertBoard(board);
         return "redirect:/board/openBoardList.do";
+    }
+
+    @RequestMapping("/board/openBoardDetail.do")
+    public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception {
+        ModelAndView mv = new ModelAndView("/board/boardDetail");
+
+        BoardDto board = boardService.selectBoardDetail(boardIdx);
+        mv.addObject("board", board);
+
+        return mv;
     }
 }
--- src/main/java/com/example/springboard/board/service/BoardService.java
+++ src/main/java/com/example/springboard/board/service/BoardService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface BoardService {
     List<BoardDto> selectBoardList() throws Exception;
     void insertBoard(BoardDto board) throws Exception;
+    BoardDto selectBoardDetail(int boardIdx) throws Exception;
 }
