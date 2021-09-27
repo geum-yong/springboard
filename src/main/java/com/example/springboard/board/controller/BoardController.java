db3b02ba64ab6a173b94df63dce20b3c439f7419
geum-yong <gmyong21@gmail.com>
2021-09-27 19:53:38 +0900

    로그 구현

--- src/main/java/com/example/springboard/board/controller/BoardController.java
+++ src/main/java/com/example/springboard/board/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.example.springboard.board.controller;
 
 import com.example.springboard.board.dto.BoardDto;
 import com.example.springboard.board.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +13,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 public class BoardController {
-
     @Autowired
     private BoardService boardService;
 
     @RequestMapping("/board/openBoardList.do")
     public ModelAndView openBoardList() throws Exception {
+        log.debug("openBoardList");
+
         ModelAndView mv = new ModelAndView("/board/boardList");
 
         List<BoardDto> list = boardService.selectBoardList();
